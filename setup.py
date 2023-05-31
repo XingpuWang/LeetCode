6c72bcd3a4a00f83d478aca46eca1422900d47c6
Xingpu Wang <xingpuwang@gmail.com>
2023-05-31 11:28:36 +0800

    Update Cython for Windows

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ from Cython.Build import cythonize
 
 # setup(ext_modules=cythonize("/Users/xingpuwang/PycharmProjects/LeetCode/dfs/FDM.pyx"))
 # setup(ext_modules=cythonize("/Users/xingpuwang/PycharmProjects/LeetCode/bruto/bruto.pyx"))
-setup(ext_modules=cythonize("/Users/xingpuwang/PycharmProjects/LeetCode/dfs_no_prune/FDM_no_prune.pyx"))
+# setup(ext_modules=cythonize("/Users/xingpuwang/PycharmProjects/LeetCode/dfs_no_prune/FDM_no_prune.pyx"))
+setup(ext_modules=cythonize("C:/Users/xingp/Documents/Work Space/LeetCode/dfs_for_win/FDM.pyx"))
